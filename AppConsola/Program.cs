7a71ef08679aa0238b4725fdda56546169147876
Tania <tania.billordo@cablasociados.com>
2026-04-29 17:03:44 -0300

    Fix: moviendo el código adentro del Main

--- AppConsola/Program.cs
+++ AppConsola/Program.cs
@@ -7,8 +7,8 @@ namespace AppConsola
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Proyecto con CI configurado por Tania Billordo");
         }
     }
 }
 
-Console.WriteLine("Proyecto con CI configurado por Tania Billordo");
